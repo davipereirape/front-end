4a690197bb7998615d1bc9641f8abcedfba077a5
Davi Pereira <pereiradavipe@gmail.com>
2017-06-17 18:17:02 -0300

    javascript-ES6-orientadado-a-objeto Encapsulamento de um objeto

--- javascript-ES6-orientado-a-objeto/client/js/app/models/Negociacao.js
+++ javascript-ES6-orientado-a-objeto/client/js/app/models/Negociacao.js
@@ -8,13 +8,36 @@ class Negociacao
 {
     constructor(data, quantidade, valor)
     {
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = new Date(data.getTime()); // Novo objeto, para evitar alterações por refência (no frezze deep)
+        this._quantidade = quantidade;
+        this._valor = valor;
+
+        // Congela o objeto, impedindo alterações nele. Pode checar se um objeto está congelado através do método
+        // Object.isFreeze(objeto); O freeze NÃO faz um deep frezer
+        Object.freeze(this);
     }  
 
-    obtemVolume() 
+    // get permite acesso ao objeto, de forma objeto.metodo , retornando o valor do return e evitando 
+    // edição do atributo da classe.
+    get volume() 
     {
-        return this.quantidade * this.valor;
+        return this._quantidade * this._valor;
     } 
+
+    get data()
+    {
+        // retorna um novo objeto para evitar à alteração no objeto da negociação. Através da referência da 
+        // data. (no freezer deep)
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade()
+    {
+        return this._quantidade;
+    }
+
+    get valor()
+    {
+        return this._valor;
+    }
 }
